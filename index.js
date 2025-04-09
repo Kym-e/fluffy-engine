9e16358fd24f72b0c81d494c64c1fbb3ce0a3010
kym hannah <louderplz88@gmail.com>
2025-04-09 19:18:00 +0100

    Starting a Node project

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+
+require('dotenv').config() // loads data from .env file
+
+const cookieParser = require('cookie-parser')
+app.use(cookieParser())
+
+app.use(express.urlencoded({
+    extended: true
+}))
+
+const path = require('path');
+const public_dir = path.join(__dirname,'public');
+app.use(express.static(public_dir));
+
+// CSS? Tailwind
+
+const mustache = require('mustache-express');
+app.engine('mustache', mustache());
+app.set('view engine', 'mustache');
+
+const router = require('./routes/danceAppRoutes');
+app.use('/', router);
+
+app.listen(process.env.PORT ||3000, () => {
+    console.log('Server started. Ctrl^c to quit.\n http://localhost:3000');
+})
